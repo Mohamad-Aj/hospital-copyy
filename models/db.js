ba36cb16c02a91e0503ee7fd5fe472a7780e339c
Mohamad-Aj <mohamadaj310@gmail.com>
2022-12-12 18:56:23 +0200

    update

--- models/db.js
+++ models/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 // const Nurse = require('../models/nurse')
 
 
-const dbURI = 'mongodb+srv://mohamad_aj3:alonssael12A@cluster0.jtnxgjr.mongodb.net/Hospital?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log('connected to db'))
     .catch((err) => console.log(err));
 
